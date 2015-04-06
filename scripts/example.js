4c6e9f98c880748e4e24606a5f4ebd9917b07993
BlueMir <qa22ahj@gmail.com>
2015-04-06 19:38:15 +0900

    add example.js

---
+++ scripts/example.js
@@ -0,0 +1,20 @@
+// Description:
+//   Example scripts for you to examine and try out.
+//
+// Notes:
+//   They are commented out by default, because most of them are pretty silly and
+//   wouldn't be useful and amusing enough for day to day huboting.
+//   Uncomment the ones you want to try and experiment with.
+//
+//   These are from the scripting documentation: https://github.com/github/hubot/blob/master/docs/scripting.md
+//
+
+module.exports = function(robot){
+  // robot.hear(/배고파/i, function(res){
+  //   res.send("밥 먹어!");
+  // });
+  //
+  // rebot.respond(/do you want to build snowman\?/i, function(res){
+  //   res.reply("go away!");
+  // });
+}
