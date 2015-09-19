346c498e060ea8466f3f3ebf5d0fe9f80efc5c62
BlueMir <qa22ahj@naver.com>
2015-09-19 17:14:32 +0900

    Add door open script

---
+++ scripts/door-open.js
@@ -0,0 +1,38 @@
+// Description:
+//   ZeroPage Room Door open
+// Commands:
+//   hubot 문열어 - Open Door
+//   hubot open door - Open Door
+//
+var address = process.env.HUBOT_DOOR_OPEN_SERVER_ADDR
+var id = process.env.HUBOT_DOOR_OPEN_SERVER_ID
+
+module.exports = function(robot){
+  robot.respond(/문열어/i, openDoor);
+  robot.respond(/open door/i, openDoor);
+  robot.respond(/knok knok/i, openDoor);
+  function openDoor(res){
+
+    robot.http(address)
+    .header("Content-Type", "application/x-www-form-urlencoded")
+    .post(function(err, req){
+        if(err) {
+          res.send("something's wrong");
+          return;
+        }
+        req.end("id=" + id);
+      })(function(err, resp, body){
+      if(err) {
+        res.send("에러났다.");
+        return;
+      }
+      if(body === "Ok"){
+        res.send("열렸다.");
+      } else {
+        res.send("열린.... 건가?");
+      }
+    });
+    res.send("열어는 드릴께");
+  }
+}
+
--- scripts/help.js
+++ scripts/help.js
@@ -3,7 +3,7 @@
 //
 
 module.exports = function(robot){
-  robot.hear(/help/i, function(res){
+  robot.respond(/help/i, function(res){
     res.send("ZeroBot은 hubot을 기반으로 한 robot입니다.");
     res.send("Repository : https://github.com/zeropage/zerobot");
     res.send("기능 추가 및 버그 수정은 github Repository에 Pull Request로 보내주시기 바랍니다.");
